d2880bc4a51f15ababc2b574f457340473fdb59c
Isael <iisael090@gmail.com>
2024-12-20 05:15:53 -0800

    modified:   next.config.ts 	modified:   package.json

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
-  output: "export", // Ensures static export
-  assetPrefix: isProd ? "/portfolio/" : "", // Set the base path
-  basePath: isProd ? "/portfolio" : "", // Base path for GitHub Pages
+  /* config options here */
 };
 
 export default nextConfig;
